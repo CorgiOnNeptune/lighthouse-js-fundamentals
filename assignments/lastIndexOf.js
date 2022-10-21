4b23e878fb262871e699ad19ae5195351fbc5c1d
Justin <justinjdbennett@gmail.com>
2022-10-21 01:13:34 +0000

    lastindexof function

---
+++ assignments/lastIndexOf.js
@@ -0,0 +1,20 @@
+// Return index of value's last occurence in an array
+
+function lastIndexOf(array = [], value) {
+  let lastIndex = -1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      lastIndex = i;
+    }
+  }
+
+  return lastIndex;
+}
+
+// Verify results
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
